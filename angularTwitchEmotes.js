e889ea2950f2f34d98ed63aa5f2ba81dc9950f0e
Jeremy Stover(Kadajett) <jeremy.ryan.stover@gmail.com>
2015-12-02 17:40:56 -0800

    working on the private service

--- angularTwitchEmotes.js
+++ angularTwitchEmotes.js
@@ -1,4 +1,22 @@
-angular.module('kadajett.ngTwitchEmote', [])
-  .directive('ngTwitchEmote', function(){
+angular.module('kadajett.ngTwitchPrivate', [])
+  .factory('Emotes', function($http, $q){
+    var api = {
+      getEmoteList:
+    };
+    function getEmoteList(){
+      var defer = $q.defer();
 
+      $http.get('http://twitchemotes.com/api_cache/v2/global.json')
+        .success(function(res){
+          defer.resolve(res);
+        })
+
+      return defer.promise;
+    }
   });
+angular.module('kadajett.ngTwitchEmote', ['kadajett.ngTwitchPrivate'])
+  .directive('ngTwitchEmote', function(){
+    function link(scope, elem, attrs){
+      
+    }
+  })
